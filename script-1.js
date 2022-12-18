e3dc465b0840325e18f163038b02311d62604536
Oleksii <al.dimitrash@gmail.com>
2022-12-18 05:52:08 +0200

    update

--- script-1.js
+++ script-1.js
@@ -1,3 +1,5 @@
 const age = 10;
 const totalPrice = 200;
 console.log(totalPrice);
+const message = `You picked ${name}, price per item is ${price} credits`;
+const canAccessContent = (subType = pro || subType = vip );
